e2d3341252119674027b612df7eec885504e3d43
Maha Lakshmi <maha6095752@gmail.com>
2025-08-30 11:14:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css';
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font bold underline text-blue-500'>Hello world </h1>
+    </div>
+  )
+}
+
+export default App
